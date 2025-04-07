56e7d45ae9e36cfa34a1c69d38650774719990eb
alokishere <alokvishwakarmabdh@gmail.com>
2025-04-07 23:11:04 +0530

    Insertion sort

--- arr.js
+++ arr.js
@@ -37,3 +37,15 @@ for (let i = 0; i < arr.length - 1; i++) {
 
 console.log(arr);
 */
+
+
+//Insertion sort
+for(let i = 1;i <arr.length;i++){
+    let key = arr[i]
+    let j = i-1
+    while(j>=0 && arr[j]>key){
+        arr[j+1] = arr[j]
+        j--
+    }
+}
+console.log(arr);
