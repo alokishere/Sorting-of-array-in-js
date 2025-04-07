c147e7ebdb2d0c1e37bdf206857cf941354003a7
alokishere <alokvishwakarmabdh@gmail.com>
2025-04-07 23:01:46 +0530

    selection sort

--- arr.js
+++ arr.js
@@ -18,3 +18,22 @@ for(let i = 0;i<n-1;i++){
 console.log(arr);
 
 */
+
+//selection sort
+/*
+for (let i = 0; i < arr.length - 1; i++) {
+    let small = i;
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[small] > arr[j]) {
+            small = j
+        }
+    }
+    if (i != small) {
+        let temp = arr[i]
+        arr[i] = arr[small]
+        arr[small] = temp
+    }
+}
+
+console.log(arr);
+*/
